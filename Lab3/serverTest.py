ced357503f28736db5e814e28c387a3da0a5b286
saachibhayani <43556163+saachibhayani@users.noreply.github.com>
2025-11-19 16:36:27 -0800

    added soil
    added soil

--- Lab3/serverTest.py
+++ Lab3/serverTest.py
@@ -1,16 +1,16 @@
 import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Assigns a port for the server that listens to clients connecting to this port.
-serv.bind((’0.0.0.0’, 8080))
+serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
     conn, addr = serv.accept()
-    from_client = ’’
+    from_client = ''
     while True:
         data = conn.recv(4096)
         if not data: break
-        from_client += data.decode(’utf_8’)
+        from_client += data.decode('utf_8')
         print(from_client)
         conn.send("I am SERVER\n".encode())
     conn.close()
-    print(’client disconnected’)
+    print('client disconnected')
